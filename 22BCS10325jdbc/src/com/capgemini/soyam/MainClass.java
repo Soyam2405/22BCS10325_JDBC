3e5ab19937eb506a69c8af6ad33ccb1bc8363158
hp <hp@DESKTOP-I3D48G7>
2024-02-08 21:26:40 +0530

    Commit

---
+++ 22BCS10325jdbc/src/com/capgemini/soyam/MainClass.java
@@ -0,0 +1,10 @@
+package com.capgemini.soyam;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("My UID IS 22BCS10325");
+	}
+
+}
