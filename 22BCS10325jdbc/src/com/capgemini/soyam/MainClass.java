92950afd0d5a0116d2c5380fd4dbd1db6d1d6ebf
hp <hp@DESKTOP-I3D48G7>
2024-02-08 23:34:21 +0530

    commit

--- 22BCS10325jdbc/src/com/capgemini/soyam/MainClass.java
+++ 22BCS10325jdbc/src/com/capgemini/soyam/MainClass.java
@@ -1,10 +1,20 @@
 package com.capgemini.soyam;
 
-public class MainClass {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-       System.out.println("My UID IS 22BCS10325");
+public class MainClass {
+	public static void main(String[] args) throws ClassNotFoundException,SQLException {
+
+
+//	public Connection getConnection() throws ClassNotFoundException,SQLException {
+		Connection con = null;
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		con = DriverManager.getConnection("jdbc:mysql://localhost:3306/guptaji","root","Qwerty@098");
+		System.out.println("Connection Established");
+		
+
 	}
 
 }
