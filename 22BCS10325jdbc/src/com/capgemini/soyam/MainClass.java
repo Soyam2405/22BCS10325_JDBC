0d0da3e3ca5e14ff54c74f0703de9654e3e91876
hp <hp@DESKTOP-I3D48G7>
2024-02-09 10:10:10 +0530

    Commit Lab evaluation 1

--- 22BCS10325jdbc/src/com/capgemini/soyam/MainClass.java
+++ 22BCS10325jdbc/src/com/capgemini/soyam/MainClass.java
@@ -1,20 +1,50 @@
 package com.capgemini.soyam;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
-
+import java.util.Scanner;
 public class MainClass {
+	
 	public static void main(String[] args) throws ClassNotFoundException,SQLException {
-
-
-//	public Connection getConnection() throws ClassNotFoundException,SQLException {
-		Connection con = null;
-		Class.forName("com.mysql.cj.jdbc.Driver");
-		con = DriverManager.getConnection("jdbc:mysql://localhost:3306/guptaji","root","Qwerty@098");
-		System.out.println("Connection Established");
 		
 
+		Scanner sc = new Scanner(System.in); // new - dynamic memory allocator.
+//		ConnectionClass object = new ConnectionClass();
+		OperationClass object = new OperationClass();
+		while(true) {
+		
+			System.out.println("Enter 1 for insert a Record");
+			System.out.println("Enter 2 for delete a Record");
+			System.out.println("Enter 3 for update a Record");
+			System.out.println("Enter 4 for displayAll a Record");
+			System.out.println("Enter 5 for displayOne a Record");
+			
+			int reply = sc.nextInt();
+			
+			switch(reply) {
+			case 1:
+				object.insert_record();
+				break;
+			case 2:
+				object.delete();
+				break;
+			case 3:
+				object.update();
+				break;
+			case 4:
+				object.display_All();
+				break;
+			case 5:
+				object.display_one();
+				break;
+			case 6:
+				break;
+			default:
+				System.out.println("Invalid Input");
+					
+			}
+		}
+		
+		
+		
 	}
 
 }
